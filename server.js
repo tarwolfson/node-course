505ada4153677376b2b6c136ea301df35d6dcf81
תר וולפסון <tarw@auth.ad.bgu.ac.il>
2018-10-10 15:47:53 +0300

    added projects 2

--- server.js
+++ server.js
@@ -39,6 +39,13 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'Projects here: '
+    })
+});
+
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
